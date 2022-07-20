c6c7c16ec47af6cb359b1f8e8d6ae4768d868866
Vladimir Timofeenko <id@vtimofeenko.com>
2022-07-20 14:03:24 -0700

    feat: add xdg desktop entry for Nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.05";
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    # File-specific fetching available from Nix 2.9
+    # icon = {
+    #   url = "file+https://raw.githubusercontent.com/Donnnno/Arcticons/ca3fd9e668a14b478dbe7ed9476e1dbe19742d63/icons/black/editor.svg";
+    #   flake = false;
+    # };
   };
 
   outputs = { self, nixpkgs, nix-doom-emacs }: {
@@ -30,6 +35,14 @@
           nixpkgs-fmt
         ];
         home.file.".aspell.conf".text = "data-dir ${pkgs.aspell}/lib/aspell";
+        xdg.desktopEntries = {
+          emacs = {
+            name = "Emacs";
+            genericName = "Text editor";
+            exec = "emacs";
+            #icon = "${icon}";
+          };
+        };
       })
       ({ ... }: {
         programs.doom-emacs = {
