fe9a647545809d3507c4f93ad985a2d65a059f72
Vladimir Timofeenko <id@vtimofeenko.com>
2022-09-16 18:31:20 -0700

    fix: correct name

--- doom.d/config.el
+++ doom.d/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Vladimmir Timofeenko"
+(setq user-full-name "Vladimir Timofeenko"
       user-mail-address "id@vtimofeenko.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
